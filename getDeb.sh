fa2429004e31633ba2dfe65906f903a4587158d1
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2018-12-06 22:36:46 +0200

    Almost done

--- getDeb.sh
+++ getDeb.sh
@@ -1,3 +1,4 @@
+cache delete $SEMAPHORE_PROJECT_NAME
 mkdir packages
 wget http://ge.archive.ubuntu.com/ubuntu/pool/universe/e/enscript/enscript_1.6.5.90-3_amd64.deb -O ./packages/enscript.deb
 cache store $SEMAPHORE_PROJECT_NAME packages
