5efda3242cd778e49bfa1342e3946fa266101c3d
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2018-12-06 22:28:14 +0200

    Directory name is packages

--- getDeb.sh
+++ getDeb.sh
@@ -1,3 +1,3 @@
-mkdir $SEMAPHORE_PROJECT_NAME
-wget http://ge.archive.ubuntu.com/ubuntu/pool/universe/e/enscript/enscript_1.6.5.90-3_amd64.deb -O ./$SEMAPHORE_PROJECT_NAME/enscript.deb
-cache store $SEMAPHORE_PROJECT_NAME $SEMAPHORE_PROJECT_NAME
+mkdir packages
+wget http://ge.archive.ubuntu.com/ubuntu/pool/universe/e/enscript/enscript_1.6.5.90-3_amd64.deb -O ./packages/enscript.deb
+cache store $SEMAPHORE_PROJECT_NAME packages
